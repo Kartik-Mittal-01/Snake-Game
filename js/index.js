f555e1f748b918e74e808cd551614abea36dd105
KartikMittal <kartikmittal8998@gmail.com>
2024-08-22 21:58:50 +0530

    speed change

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 
-let speed = 19;
+let speed = 18;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
